efdaba79ff3fc27c6844222ccf9f1b4ea448be16
karikaripan <flowerk@gmail.com>
2025-11-29 20:08:02 +0000

    Finalização

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,29 @@ class ProductsController < ApplicationController
         @products = Product.all 
     end
     def show
+        @product = Product.find(params[:id])
+    end
+    def new
+        @product = Product.new
+    end
+    def create
+        @product = Product.new(name: params[:product][:name], price: params[:product][:price], quantity: params[:product][:quantity])
+        @product.save
+        redirect_to @product
+    end
+    def edit 
+        @product = Product.find(params[:id])
+    end
+    def update
+        @product = Product.find(params[:id])
+        @product.update(name: params[:product][:name], price: params[:product][:price], quantity: params[:product][:quantity])
+        redirect_to @product
+    end
+
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+
+        redirect_to products_path, status: :see_other
     end
 end
